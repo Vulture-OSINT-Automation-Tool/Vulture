611801c2c347239c8f62af989b92de984934cd8c
Ryan Voit <93566632+Ghost7926@users.noreply.github.com>
2024-04-15 21:42:14 -0700

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,5 +0,0 @@
-# Place Keys here
-hunter_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # Hunter.io API Key
-dehashed_cred_key = 'XXXXXXXXXXX@YYYYYYYYYYY.com' # Dehashed email
-dehashed_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # Dehashed API Key
-whois_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # Whois API Key
